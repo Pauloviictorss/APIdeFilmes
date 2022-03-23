52c9b80594157089d64f7a0ff9580e678a1af44b
“Pauloviictorss” <paulovictorlvm@gmail.com>
2022-03-22 22:24:43 -0300

    First Commit

---
+++ src/components/Menu.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Menu = () => {
+  return (
+    <div>
+        <h1>Teste</h1>
+        <ul>
+            <li><a href="">Página 1</a></li>
+            <li><a href="">Página 2</a></li>
+            <li><a href="">Página 3</a></li>
+            <li><a href="">Página 4</a></li>
+        </ul>
+
+    </div>
+  )
+}
+
+export default Menu
